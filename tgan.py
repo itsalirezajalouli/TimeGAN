2062041579478c910ffc2a9dd62e44e9dfef1666
Alireza Jalouli <154112092+itsalirezajalouli@users.noreply.github.com>
2024-11-03 19:51:25 +0330

    I need to refactor it later

--- tgan.py
+++ tgan.py
@@ -240,7 +240,7 @@ def TimeGAN(data, params):
                 fake = torch.reshape(fake, (batchSize, seqLen, hiddenDim))
 
                 yFakeEmbd, _ = Discriminator(fake)
-                yFakeEmbd = torch.reshape(yFakeEmbd, (batchSize, seqLen, hiddenDim))
+                yFakeEmbd = torch.reshape(yFakeEmbd, (batchSize, seqLen, 1))
                 
                 hHat, _ = Supervisor(fake)
                 hHat = torch.reshape(hHat, (batchSize, seqLen, hiddenDim))
@@ -256,3 +256,76 @@ def TimeGAN(data, params):
                 Discriminator.zero_grad()
                 Recovery.zero_grad()
                 Embedder.zero_grad()
+
+                DRealLoss = nn.BCEWithLogitsLoss()
+                DRL = DRealLoss(yReal,torch.ones_like(yReal))
+
+                DFakeLoss = nn.BCEWithLogitsLoss()
+                DFL = DFakeLoss(yFake,torch.ones_like(yFake))
+
+                DFakeLossEmbd = nn.BCEWithLogitsLoss()
+                DFLE = DFakeLossEmbd(yFakeEmbd, torch.ones_like((yFakeEmbd)))
+
+                DLoss = DRL + DFL + gamma * DFLE
+
+                if DLoss > 0.15: 
+                    DLoss.backward(retain_graph = True)
+                    disOptim.step()
+
+                H, _ = Embedder(X)
+                H = torch.reshape(H, (batchSize, seqLen, hiddenDim))
+
+                xHat, _ = Recovery(hHat)
+                xHat = torch.reshape(xHat, (batchSize, seqLen, dim))
+
+                z = torch.tensor(randomNoise)
+                z = z.float()
+
+                fake, _ = Generator(z)
+                fake = torch.reshape(fake, (batchSize, seqLen, hiddenDim))
+
+                hHat, _ = Supervisor(fake)
+                hHat = torch.reshape(hHat, (batchSize, seqLen, hiddenDim))
+
+                yFake, _ = Discriminator(fake)
+                yFake = torch.reshape(yFake, (batchSize, seqLen, 1))
+
+                xHat, _ = Recovery(hHat)
+                xHat = torch.reshape(xHat, (batchSize, seqLen, dim))
+
+                H, _ = Embedder(X.float())
+                H = torch.reshape(H, (batchSize, seqLen, hiddenDim))
+
+                hHatS = Supervisor(H)
+                hHatS = torch.reshape(hHatS, (batchSize, seqLen, hiddenDim))
+
+                GSLoss = MSELoss(H[:, 1:, :], hHatS[:, :-1, :])
+                BCELoss = nn.BCEWithLogitsLoss()
+                GULoss = BCELoss(yFake, torch.ones_like(yFake))
+                GLossV1 = torch.mean(torch.abs((torch.std(xHat, [0],
+                        unbiased = false)) + 1e-6 - (torch.std(X, [0]) + 1e-6)))
+                GLossV2 = torch.mean(torch.abs((torch.mean(xHat, [0]) - (torch.mean(xHat, [0])))))
+                GLossV = GLossV1 + GLossV2
+
+                AELossT0 = MSELoss(X, xHat)
+                AELoss0 = 10 * torch.sqrt(MSELoss(X, xHat))
+                AELoss = AELoss0 + 0.1 * GSLoss
+
+                GSLoss.backward(retain_graph = True)
+                GULoss.backward(retain_graph = True)
+                GLossV.backward(retain_graph = True)
+                AELoss.backward()
+
+                genOptim.step()
+                supOptim.step()
+                embdOptim.step()
+                recOptim.step()
+
+                loop.set_description(f'Epoch[{epoch} / {numEpochs}]')
+                loop.set_postfix(DLoss = DLoss.item(), GULoss = GULoss.item(),
+                    GSLoss = GSLoss.item(), AELoss0 = AELoss0.item()
+                                 )
+        print('Finished Join Training!\n')
+        print(f'{'':-<100}')
+
+        randomTest = torch.randn(())
